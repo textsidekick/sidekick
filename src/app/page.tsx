83e3e344fd4e7f3c5b8c47c10a103b9a49d06325
JaySo13 <so.justin8@gmail.com>
2026-04-29 17:23:51 -0700

    Fix: server-side redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from 'next/navigation';
-import { Effect } from 'react';
 
-// Redirect to login page
 export default function Page() {
-  Effect(() => {
-    redirect('/login');
-  }, []);
-
-  return null;
+  redirect('/login');
 }
