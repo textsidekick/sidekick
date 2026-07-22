d2df7cf9c2b6de25801f618a4bda6e2cac4a3550
Justin So <so.justin8@gmail.com>
2026-07-22 16:17:10 -0400

    Fix hotel overview runtime crash

--- src/app/hotel/page.tsx
+++ src/app/hotel/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { BedDouble, DoorOpen, Wrench } from "lucide-react";
 import { HotelPageHeader, HotelStatusPill } from "@/components/hotel/HotelUi";
 import { useHotelDemoState } from "@/lib/hotel-demo-store";
